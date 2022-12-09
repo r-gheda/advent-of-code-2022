1401e9fca75b6b4b7f5c1b342ad9d6ecd6450fc7
r-gheda <robertogheda.rg@gmail.com>
2022-12-09 17:51:32 +0100

    Day 6 Problem 1

---
+++ day-06/day06-1/src/main.rs
@@ -0,0 +1,36 @@
+use std::fs;
+use std::collections::HashSet;
+
+fn main()
+{
+    let file = fs::read_to_string("input.txt")
+        .expect("Failed to open file");
+
+    let mut counter = 0;
+    let packet_dim = 4;
+
+    for _ in file.chars()
+    {
+        let mut chars: HashSet<char> = HashSet::new();
+        let mut is_packet = true;
+
+        for i in 0..packet_dim
+        {
+            if i > counter { is_packet = false; break; }
+            let ch = file.chars().nth(counter-i);
+            match ch
+            {
+                Some(ch) => {
+                    if !chars.contains(&ch) { chars.insert(ch); } 
+                    else { is_packet = false; break; }
+                },
+
+                None => panic!("No charachter read")
+            }
+        }
+        counter = counter + 1;
+        if is_packet { break; }
+    }
+
+    println!("Solution: {}", counter);
+}
